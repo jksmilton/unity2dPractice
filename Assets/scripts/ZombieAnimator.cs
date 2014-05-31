20b5d06559cca52b45cafbe811efabf2582bd0c9
Jordan Milton <jksmilton@gmail.com>
2014-05-31 17:48:03 +0100

    first commit

---
+++ Assets/scripts/ZombieAnimator.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieAnimator : MonoBehaviour {
+	public Sprite[] sprites;	
+	public float framesPerSecond;
+
+	private SpriteRenderer renderOb;
+
+	// Use this for initialization
+	void Start () {
+		renderOb = renderer as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = (int)(Time.timeSinceLevelLoad * framesPerSecond);
+		index = index % sprites.Length;
+		renderOb.sprite = sprites [index];
+	}
+
+
+
+
+
+}
